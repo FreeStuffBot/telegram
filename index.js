0c5ed45df92ed28ddcb81d3064a3cf6837295fc1
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-20 12:02:22 +0200

    Create an API instance and test it

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ const config = require('./config');
  */
 const { Telegraf } = require('telegraf');
 
+/**
+ * The FreeStuff API wrapper, huge thanks for maanex!
+ */
+const { FreeStuffApi } = require("freestuff");
+
 const bot = new Telegraf(config.telegramToken);
+
+const api = new FreeStuffApi({
+    type: config.freeStuffAPI.partner ? 'partner' : 'basic',
+    key: config.freeStuffAPI.token,
+    sid: 'DEV_' + Math.floor(10 + Math.random() * 100000),
+    version: '0.0.1'
+});
+
+api.ping().then(() => console.log('API PONG!')).catch(() => console.log('failed to ping the api...'));
 
 bot.start((ctx) => ctx.reply('Welcome, the bot is still WIP'));
 bot.help((ctx) => ctx.reply('The bot is still WIP'));
