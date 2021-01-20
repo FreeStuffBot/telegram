7d7d6687e37611236a41aba0958303638b69c0c7
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-20 11:20:44 +0200

    Add example bot code

---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * The bot configuration file.
+ * Check the template file: ./config.template.js
+ */
+const config = require('./config');
+
+/**
+ * The telegram bots library used.
+ */
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(config.telegramToken);
+
+bot.start((ctx) => ctx.reply('Welcome, the bot is still WIP'));
+bot.help((ctx) => ctx.reply('The bot is still WIP'));
+bot.ping((ctx) => ctx.reply('Pong 🏓'));
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
