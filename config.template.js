c612061117e45cbb18833b9f56b540c0899405fb
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-20 11:02:03 +0200

    Add a configuration file

---
+++ config.template.js
@@ -0,0 +1,7 @@
+module.exports = {
+    telegramToken: "The bot token provided by @BotFather on Telegram",
+    freeStuffAPI: {
+        token: "The freestuff api token",
+        partner: false //Whether this is a partner token or not
+    }
+}
