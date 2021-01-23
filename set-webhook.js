9410a34c31cc54385d0c5e6dbfb2e4b4900dd2e1
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-23 16:21:28 +0200

    Added scripts to manage the Telegram webhook

---
+++ set-webhook.js
@@ -0,0 +1,9 @@
+const config = require('./config');
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(config.telegram.token);
+const domainName = 'https://freestuff-telegram.vercel.app';
+
+bot.telegram.setWebhook(`${domainName}/api/${config.telegram.token}/telegram_webhook`)
+    .then(() => console.log('The webhook has been set successfully ✅'))
+    .catch(console.error);
