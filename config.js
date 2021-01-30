36f5076c81a86a6f052ae3914b0df640010015be
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-30 22:38:10 +0200

    Configured the top 10 languages we have

--- config.js
+++ config.js
@@ -26,12 +26,31 @@ module.exports = {
         },
 
         announcements: {
-            ['en-US']: '@freestuff_english_us_561020',
-            ['ar-SY']: '@freestuff_arabic_936767',
-            ['de-DE']: '@freestuff_german_370794',
-            
+            ['en-US']: '@freestuff_english_us_561020', //https://t.me/freestuff_english_us_561020
+            ['en-GB']: '@freestuff_english_gb_637292', //https://t.me/freestuff_english_gb_637292
+            ['pt-BR']: '@freestuff_portuguese_brazil_797843', //https://t.me/freestuff_portuguese_brazil_797843
+            ['es-ES']: '@freestuff_spanish_latam_764053', //https://t.me/freestuff_spanish_latam_764053
+            ['tr']: -1001221998355, //https://t.me/joinchat/SNY3E9OOi8Jn1mO3
+            ['it-IT']: -1001276996569, //https://t.me/joinchat/TB1r2WpVKsZIoeFr
+            ['de-DE']: '@freestuff_german_370794', //https://t.me/freestuff_german_370794'
+            ['fr-FR']: -1001256507523, //https://t.me/joinchat/SuTIg0WrXoVHNE4M
+            ['ar-SY']: '@freestuff_arabic_936767', //https://t.me/freestuff_arabic_936767
+            ['pl']: -1001459962181, //https://t.me/joinchat/VwVBRd2kdf0onVUz
+
         }
     },
 
-    languages: [ 'en-US', 'ar-SY', 'de-DE' ] //It's okay if a language is missing from the configured channels list.
+    //It's okay if a language is missing from the configured channels list.
+    languages: [
+        'en-US',
+        'en-GB',
+        'pt-BR',
+        'es-ES',
+        'tr',
+        'it-IT',
+        'de-DE',
+        'fr-FR',
+        'ar-SY',
+        'pl',
+    ]
 }
