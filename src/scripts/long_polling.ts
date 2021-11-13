c30b134f609d03f67974c820d5d5236796178563
Rami <ramilego4game@gmail.com>
2021-11-13 23:09:36 +0200

    Be less verbose with the logging

--- src/scripts/long_polling.ts
+++ src/scripts/long_polling.ts
@@ -1,13 +1,10 @@
-
-console.info('> Loading .env file...');
 import { config as loadENV } from 'dotenv';
 loadENV();
 
-console.info('> Launching the bot....');
 import { bot } from '../lib/telegram_bot';
 bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-console.info('> Ready ✔️');
+console.info('Ready.');
